b2a35b5f09980fe12e5f348d32d1da8fcd8d6d29
fushang318 <fushang318@gmail.com>
2021-06-19 14:35:08 +0800

    fix: get_next_time

--- utils/__init__.py
+++ utils/__init__.py
@@ -44,33 +44,6 @@ def get_next_time(time_zone, last_at, time_day, time_hour):
     return next_at
 
 
-# def get_next_time(time_zone, last_at, time_day, time_hour):
-#     """
-#     time_zone 是时区数字
-#     last_at utc 时间戳
-#     time_day time_zone 下的时间 天
-#     time_hour time_zone 下的时间 小时
-#     result: time_zone 下下一个 time_day time_hour 具体的 UTC 时间戳
-#     """
-#     last_time = datetime.datetime.fromtimestamp(last_at + time_zone * 60)
-#     if time_day > last_time.day or (
-#             time_day == last_time.day and time_hour > last_time.hour):
-#         next_time = datetime.datetime.strptime(
-#             f"{last_time.year}-{last_time.month}-{time_day} {time_hour}",
-#             "%Y-%m-%d %H"
-#         )
-#     else:
-#         next_month = datetime.datetime(
-#             last_time.year, last_time.month,
-#             calendar.monthrange(last_time.year, last_time.month)[1]) + datetime.timedelta(days=1)
-#         next_time = datetime.datetime.strptime(
-#             f"{next_month.year}-{next_month.month}-{time_day} {time_hour}",
-#             "%Y-%m-%d %H"
-#         )
-#     next_at = next_time.timestamp() - time_zone * 60
-#     return next_at
-
-
 if __name__ == '__main__':
     check_size(2.33)
     check_size(2.3)
