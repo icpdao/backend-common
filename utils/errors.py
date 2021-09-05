5678903ae60519a1719604c47cb1baec6cb895fb
bqx619 <bqx619@gmail.com>
2021-09-05 19:33:35 +0800

    feat: tucked errors

---
+++ utils/errors.py
@@ -0,0 +1,12 @@
+
+COMMON_NOT_AUTH_ERROR = 'error.common.not_login'
+COMMON_NOT_PERMISSION_ERROR = 'error.common.not_permission'
+COMMON_TIMEOUT_ERROR = 'error.common.timeout'
+
+ICPPER_NOT_FOUND_ERROR = 'error.icpper.not_found'
+ICPPER_LOOP_BACK_ERROR = 'error.icpper.loop_back'
+ICPPER_PRE_MENTOR_MAX_ERROR = 'error.icpper.max_pre_mentor'
+ICPPER_ALREADY_MENTOR_ERROR = 'error.icpper.already_mentor'
+
+USER_WALLET_FORMAT_INVALID_ERROR = 'error.user.wallet_format_invalid'
+
