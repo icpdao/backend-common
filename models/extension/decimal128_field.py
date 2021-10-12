cee3eccebcd0e46e22fc17c77addc20df6e6f82d
bqx619 <bqx619@gmail.com>
2021-10-12 23:34:57 +0800

    feat: add decimal proposal

---
+++ models/extension/decimal128_field.py
@@ -0,0 +1,54 @@
+# from: https://github.com/MongoEngine/mongoengine/pull/2521
+# proposal: implement Decimal128Field
+import decimal
+
+from bson.decimal128 import Decimal128, create_decimal128_context
+from mongoengine.fields import BaseField
+
+
+class Decimal128Field(BaseField):
+    """
+
+    128-bit decimal-based floating-point field capable of emulating decimal
+    rounding with exact precision. Stores the value as a `Decimal128`
+    intended for monetary data, such as financial, tax, and scientific
+    computations.
+    """
+
+    DECIMAL_CONTEXT = create_decimal128_context()
+
+    def __init__(self, min_value=None, max_value=None, **kwargs):
+        self.min_value = min_value
+        self.max_value = max_value
+        super().__init__(**kwargs)
+
+    def to_mongo(self, value):
+        if value is None:
+            return None
+        if isinstance(value, Decimal128):
+            return value
+        if not isinstance(value, decimal.Decimal):
+            with decimal.localcontext(self.DECIMAL_CONTEXT) as ctx:
+                value = ctx.create_decimal(value)
+        return Decimal128(value)
+
+    def to_python(self, value):
+        if value is None:
+            return None
+        return self.to_mongo(value).to_decimal()
+
+    def validate(self, value):
+        if not isinstance(value, Decimal128):
+            try:
+                value = Decimal128(value)
+            except (TypeError, ValueError, decimal.InvalidOperation) as exc:
+                self.error("Could not convert value to Decimal128: %s" % exc)
+
+        if self.min_value is not None and value.to_decimal() < self.min_value:
+            self.error("Decimal value is too small")
+
+        if self.max_value is not None and value.to_decimal() > self.max_value:
+            self.error("Decimal value is too large")
+
+    def prepare_query_value(self, op, value):
+        return super().prepare_query_value(op, self.to_mongo(value))
